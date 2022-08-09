3b58d55f5ca6b9894d6d7bff128b2cd510c5e1b7
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-09 11:02:42 -0700

    task 3

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -8,14 +8,14 @@ unsigned int convert_p(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 
 /**
- * convert_c - Converts an argument to an unsigned char and
- *             stores it to a buffer contained in a struct.
- * @args: A va_list pointing to the argument to be converted.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
- * @len: A length modifier.
- * @output: A buffer_t struct containing a character array.
+ *convert_c - Converts an argument to an unsigned char and
+ *stores it to a buffer contained in a struct.
+ *@args: A va_list pointing to the argument to be converted.
+ *@flags: Flag modifiers.
+ *@wid: A width modifier.
+ *@prec: A precision modifier.
+ *@len: A length modifier.
+ *@output: A buffer_t struct containing a character array.
  *
  * Return: The number of bytes stored to the buffer.
  */
@@ -40,14 +40,14 @@ unsigned int convert_c(va_list args, buffer_t *output,
 /**
  * convert_percent - Stores a percent sign to a
  *                   buffer contained in a struct.
- * @args: A va_list pointing to the argument to be converted.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
- * @len: A length modifier.
- * @output: A buffer_t struct containing a character array.
+ *@args: A va_list pointing to the argument to be converted.
+ *@flags: Flag modifiers.
+ *@wid: A width modifier.
+ *@prec: A precision modifier.
+ *@len: A length modifier.
+ *@output: A buffer_t struct containing a character array.
  *
- * Return: The number of bytes stored to the buffer (always 1).
+ *Return: The number of bytes stored to the buffer (always 1).
  */
 unsigned int convert_percent(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
@@ -69,14 +69,14 @@ unsigned int convert_percent(va_list args, buffer_t *output,
 /**
  * convert_p - Converts the address of an argument to hex and
  *             stores it to a buffer contained in a struct.
- * @args: A va_list pointing to the argument to be converted.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
- * @len: A length modifier.
- * @output: A buffer_t struct containing a character array.
+ *@args: A va_list pointing to the argument to be converted.
+ *@flags: Flag modifiers.
+ *@wid: A width modifier.
+ *@prec: A precision modifier.
+ *@len: A length modifier.
+ *@output: A buffer_t struct containing a character array.
  *
- * Return: The number of bytes stored to the buffer.
+ *Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_p(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
